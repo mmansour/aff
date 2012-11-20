763f5db832e6fbadc55c929e0e5d2922e7fc023b
Matt Mansour <slackbabbath@gmail.com>
2012-11-20 07:59:00 -0800

    Adding new home page

--- aff/urls.py
+++ aff/urls.py
@@ -1,2 +1,18 @@
-__author__ = 'mattmansour'
-  
+from django.conf.urls.defaults import *
+from django.contrib import admin
+admin.autodiscover()
+
+from mezzanine.core.views import direct_to_template
+
+urlpatterns = patterns('aff.views',
+    url("^$", "home", name="home"),
+#    (r'^success/$', 'success'),
+#    (r'^create-tshirt/$', 'create_shirt_form'),
+#    (r'^designer/$', 'designer'),
+#    (r'^forbidden/$', 'unauthorized'),
+#    (r'^my-tshirts/$', 'my_shirt_list'),
+#    (r'^my-tshirts/edit/(?P<shirt_id>\d+)/$', 'edit_shirt'),
+#    (r'^my-tshirts/tool-edit/(?P<shirt_id>\d+)/$', 'tool_edit'),
+
+#    (r'^my-tshirts/tool-edit/(?P<shirt_id>\d+)/(?P<shirt_color>\w+)/$', 'tool_edit'),
+)
--- aff/views.py
+++ aff/views.py
@@ -1,1 +1,19 @@
-# Create your views here.
+from aff.models import *
+from django.contrib.auth.models import User
+
+from django.shortcuts import render_to_response, get_object_or_404, get_list_or_404
+from django.template import RequestContext
+from django import forms
+from django.http import HttpResponse, Http404, HttpResponsePermanentRedirect, HttpResponseRedirect
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from django.core.mail import EmailMultiAlternatives
+from django.core.exceptions import ValidationError
+import os
+
+
+def home(request):
+    return render_to_response('index.html',{},
+                context_instance=RequestContext(request))
